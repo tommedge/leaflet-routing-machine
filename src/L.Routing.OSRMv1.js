78a3e3a8d26f4b42c7d785f6ab35da4feb41d3db
Tom Edge <tedge@mediaocean.com>
2017-03-04 18:23:36 +0000

    adding trips support

--- src/L.Routing.OSRMv1.js
+++ src/L.Routing.OSRMv1.js
@@ -133,8 +133,10 @@
 
 			actualWaypoints = this._toWaypoints(inputWaypoints, response.waypoints);
 
-			for (i = 0; i < response.routes.length; i++) {
-				route = this._convertRoute(response.routes[i]);
+            var routes = !options.trip ? response.routes : response.trips;
+
+            for (i = 0; i < routes.length; i++) {
+				route = this._convertRoute(routes[i]);
 				route.inputWaypoints = inputWaypoints;
 				route.waypoints = actualWaypoints;
 				route.properties = {isSimplified: !options || !options.geometryOnly || options.simplifyGeometry};
